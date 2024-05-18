914fbf69bf1f4b40e8bcf436f41a12fbf1d82999
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-05-18 14:02:28 -0700

    birth pr2

---
+++ pr2/lib.py
@@ -0,0 +1,12 @@
+def checkCollision(point1, point2, block):
+    lx = block[0]
+    ly = block[1]
+    lz = block[2]
+    hx = block[0]
+    hy = block[1]
+    hz = block[2]
+
+    lineDirection = point1 - point2
+    rx = lineDirection[0]
+    ry = lineDirection[1]
+    rz = lineDirection[2]
