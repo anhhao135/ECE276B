1beea21b7e6d048d2350ada0497727a6b5436385
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-06-03 20:41:40 -0700

    etc

--- hw3/my_frozen_lake.py
+++ hw3/my_frozen_lake.py
@@ -1,22 +1,5 @@
 import numpy as np
 
-class MDP(object):
-  def __init__(self, P, nX, nU, gamma = 0.95):
-    self.nX = nX
-    self.nU = nU
-    self.gamma = gamma
-    self.P = np.zeros((nX,nU,nX)) # transition probability: SxAxS' -> [0,1]
-    self.L = np.zeros((nX,nU))    # stage cost: SxA -> R
-    self.Y = np.full(nX,np.inf)   # terminal cost: S -> R       
-    for x in range(nX):
-      for u in range(nU):
-        for (pr,nx,reward,done) in P[x][u]:
-          self.P[x,u,nx] += pr
-          self.L[x,u] -= reward*pr
-          if done: self.Y[nx] = 0
-
-
-
 #controls: north (0), east (1), south (2), west (3)
 
 stateSpaceSize = 25
@@ -34,7 +17,6 @@ L[[0,5,10,15,20],3] = 1 #leftmost column move west would bring it out of map, in
 #second do special costs
 L[1,:] = -10 #special state A, all controls incur -10
 L[3,:] = -5 #special state B, all controls incur -5
-
 
 P = np.zeros((stateSpaceSize,controlSpaceSize,stateSpaceSize)) #initialize motion model matrix: P[state t,control input, state t+1] = probability of occurence
 
@@ -73,9 +55,7 @@ P[3] = 0
 P[1,:,21] = 1 #all controls from special state A (1) will take it to state A' (21) deterministically
 P[3,:,13] = 1 #all controls from special state B (3) will take it to state B' (13) deterministically
 
-
 gamma = 0.9 #discount factor
-
 
 iterations = 1000
 V = np.zeros((iterations+1, stateSpaceSize))
@@ -85,26 +65,24 @@ Q = np.zeros((iterations+1, stateSpaceSize, controlSpaceSize))
 
 for k in range(iterations):
     Q_k = L + gamma * np.sum(P * np.min(Q[k,:],axis=1), axis=2)
-    #print(Q_k.shape)
     pi[k+1,:] = np.argmin(Q_k, axis=1) #policy improvement
-    Q[k+1,:,:] = Q_k #value update
-    #print(pi[k+1])
-    #print(Q[k+1])
-    #maxValueDifference = np.abs(V[k+1] - V[k]).max()
-    #print(maxValueDifference)
+    Q[k+1,:,:] = Q_k #Q value update
+
+
+print("\n\n")
+print("optimal policy:\n")
 print(pi[-1])
-
+print("\n\n")
+print("optimal value function:\n")
 for i in range(pi[-1].shape[0]):
-    print(Q[-1,i,pi[-1,i]])
-
-#print(V[-1])
-
-
+    print(str(i) + ": " + str(Q[-1,i,pi[-1,i]]))
+print("\n\n")
 
 
 
 while False:
 
+    #policy iteration
     for k in range(iterations):
         P_pi = np.zeros((stateSpaceSize, stateSpaceSize))
         for i in range(P_pi.shape[0]):
@@ -117,26 +95,35 @@ while False:
 
         A = np.eye(stateSpaceSize) - gamma * P_pi
         b = L_pi
-        V[k] = np.linalg.solve(A, b)
-
+        V[k] = np.linalg.solve(A, b) #policy evaluation
 
         Q = L + gamma * np.sum(P * V[k,:], axis=2)
         pi[k+1,:] = np.argmin(Q, axis=1) #policy improvement
 
-        print(V[k])
-        print(pi[k])
-
+    print("\n\n")
+    print("optimal policy:\n")
+    print(pi[-1])
+    print("\n\n")
+    print("optimal value function:\n")
+    print(V[iterations - 2])
+    print("\n\n")
 
 
 while False:
 
-
+    #value iteration
     for k in range(iterations):
         Q = L + gamma * np.sum(P * V[k,:], axis=2)
         pi[k+1,:] = np.argmin(Q, axis=1) #policy improvement
         V[k+1,:] = np.min(Q, axis=1) #value update
         maxValueDifference = np.abs(V[k+1] - V[k]).max()
         print(maxValueDifference)
+
+    print("\n\n")
+    print("optimal policy:\n")
     print(pi[-1])
+    print("\n\n")
+    print("optimal value function:\n")
     print(V[-1])
+    print("\n\n")
 
