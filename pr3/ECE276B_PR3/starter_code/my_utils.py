11055512bad00e44ec5f20363515558fb5896811
Hao Le <51278143+anhhao135@users.noreply.github.com>
2024-06-11 16:13:01 -0700

    Update my_utils.py

--- pr3/ECE276B_PR3/starter_code/my_utils.py
+++ pr3/ECE276B_PR3/starter_code/my_utils.py
@@ -86,46 +86,52 @@ def GPI_controller(curTime, currentState, currentRef, policy, stateSpace, contro
 
 def NLP_controller(delta_t, horizon, traj, currentIter, currentState, freeSpaceBounds, obstacle1, obstacle2, Q_in, R_in, q_in, obstaclePadding, gamma_in):
         
+    #set up obstacle description variables
     obstacleCenter = np.array([[obstacle1[0], obstacle1[1]]]).T
     obstacleRadius = obstacle1[2]
 
     obstacleCenter2 = np.array([[obstacle2[0], obstacle2[1]]]).T
     obstacleRadius2 = obstacle2[2]
 
+    #pre-generate reference trajectory ahead by time horizon
     referenceStatesAhead = []
     for i in range(currentIter, currentIter + horizon + 1):
         referenceStatesAhead.append(traj(i))
     referenceStatesAhead = np.array(referenceStatesAhead)
 
+    #the below logic is to solve the issue of angle wrap around that can cause the NLP solver to produce extreme control inputs
+    #consider a transition from pi to -pi caused by a slight counter clockwise rotation
+    #this would shift the orientation error state suddenly by almost 2pi, causing the solver to produce extreme angular velocities to turn clockwise for the correction when in reality, we want a slight counter clockwise rotation
+    #we modify the reference state or the current state orientation component such that they are part of a continuous 1D space
     referenceStatesAhead = np.array(referenceStatesAhead)
     referenceStatesAheadThetas = referenceStatesAhead[:,2]
-    #print("current state", currentState)
-    referenceStatesAheadThetas = np.concatenate((referenceStatesAheadThetas, np.atleast_1d(currentState[2])))
-    #print(referenceStatesAheadThetas)
-    referenceStatesAheadThetas = np.unwrap(referenceStatesAheadThetas)
-    #print(referenceStatesAheadThetas)
-    referenceStatesAhead[:,2] = referenceStatesAheadThetas[:-1]
-    currentState[2] = referenceStatesAheadThetas[-1]
+    referenceStatesAheadThetas = np.concatenate((referenceStatesAheadThetas, np.atleast_1d(currentState[2]))) #add the current orientation state to the collection of orientations
+    referenceStatesAheadThetas = np.unwrap(referenceStatesAheadThetas) #unwrap ensures all the orientations follow a continuous space
+    referenceStatesAhead[:,2] = referenceStatesAheadThetas[:-1] #place continuous reference orientations back to reference poses ahead
+    currentState[2] = referenceStatesAheadThetas[-1] #place continuous current orientation back to current state
 
+    #set up NLP solver variables
     U = MX.sym('U', 2 * horizon)
     P = MX.sym('E', 2 * horizon + 2)
     Theta = MX.sym('theta', horizon + 1)
+    E_given = getError(currentState, referenceStatesAhead[0]) #get the current error state
 
-    E_given = getError(currentState, referenceStatesAhead[0])
-
-    print("error", E_given)
-
+    #define the control space boundaries
     lowerBoundv = 0
     upperBoundv = 1
     lowerBoundw = -1
     upperBoundw = 1
 
-    lowerBoundPositionError = -5
-    upperBoundPositionError = 5
+    #define the position error boundaries i.e. we are allowing the maximum position error to be 1 in magnitude
+    lowerBoundPositionError = -1
+    upperBoundPositionError = 1
 
+    #define the orientation error boundaries i.e. we are allowing the maximum orientation error to be 0.5 in magnitude
     lowerBoundOrientationError = -0.5
     upperBoundOrientationError = 0.5
 
+
+    #define the constraint vectors to be fed into the Casadi NLP solver
     controlInputSolverLowerConstraint = list(np.tile(np.array([lowerBoundv, lowerBoundw]), horizon))
     controlInputSolverUpperConstraint = list(np.tile(np.array([upperBoundv, upperBoundw]), horizon))
 
@@ -135,22 +141,24 @@ def NLP_controller(delta_t, horizon, traj, currentIter, currentState, freeSpaceB
     orientationErrorSolverLowerConstraint = list(lowerBoundOrientationError * np.ones(horizon + 1))
     orientationErrorSolverUpperConstraint = list(upperBoundOrientationError * np.ones(horizon + 1))
 
-    initialConditionSolverConstraint = list(np.zeros(5 * horizon + 3))
+    initialConditionSolverConstraint = list(np.zeros(5 * horizon + 3)) #initialize all variables to 0
     motionModelSolverConstraint = list(np.zeros((horizon+1) * 3))
 
-
-    upperBoundObstacleAvoider = 100
+    upperBoundObstacleAvoider = np.inf #there is no upper boundary for how far away the robot can be from the obstacle
 
     obstacleSolverLowerConstraint = list(np.zeros(2 * horizon))
     obstacleSolverUpperConstraint = list(upperBoundObstacleAvoider * np.ones(2 * horizon))
 
-
+    #constrain the position error to the map boundaries
     mapBoundSolverLowerConstraint = list(np.tile(np.array([freeSpaceBounds[0], freeSpaceBounds[1]]), horizon))
     mapBoundSolverUpperConstraint = list(np.tile(np.array([freeSpaceBounds[2], freeSpaceBounds[3]]), horizon))
 
 
     variables = vertcat(U, P, Theta)
 
+
+
+    #define the stage cost penalty variables in matrix form
     Q = Q_in * np.eye(2)
     QBatch = np.kron(np.eye(horizon,dtype=int),Q)
 
@@ -159,6 +167,7 @@ def NLP_controller(delta_t, horizon, traj, currentIter, currentState, freeSpaceB
 
     q = q_in
 
+    #create gammas vector and matrix
     gammaValue = gamma_in
     gammas = np.zeros(horizon)
     for i in range(horizon):
@@ -169,25 +178,31 @@ def NLP_controller(delta_t, horizon, traj, currentIter, currentState, freeSpaceB
         gammas2D[i*2:i*2+2,i*2:i*2+2] = gammas[i] * np.eye(2)
 
 
-    costFunction = (P[2*(horizon-1)]**2 + P[2*(horizon-1) + 1]**2 + Theta[horizon-1]**2) + P[:2*horizon].T @ gammas2D @ QBatch @ P[:2*horizon] + U.T @ gammas2D @ RBatch @ U + q * np.atleast_2d(gammas) @ (1 - cos(Theta[:horizon]))**2
+    #define the objective function in matrix form as the sum of the stage costs
+    #the terminal cost is simply the norm squared of the last error state i.e. we want to terminate ideally with no error
+    costFunction = (P[2*(horizon-1)]**2 + P[2*(horizon-1) + 1]**2 + Theta[horizon-1]**2) + P[:2*horizon].T @ gammas2D @ QBatch @ P[:2*horizon] + U.T @ gammas2D @ RBatch @ U + q * np.atleast_2d(gammas) @ (1 - cos(Theta[:horizon]))**2 
 
-    motionModelConstraint0 = vertcat(P[0:2], Theta[0]) - E_given
-
+    motionModelConstraint0 = vertcat(P[0:2], Theta[0]) - E_given #ensure the first state error is equal to the current error state
     g = vertcat(motionModelConstraint0)
 
+
     for i in range(horizon):
+        #define the motion model constraints
         motionModelConstraint = vertcat(P[(i+1)*2:(i+1)*2+2], Theta[i+1]) - errorMotionModelNoNoise(delta_t, P[i*2:i*2+2], Theta[i], U[i*2:i*2+2], referenceStatesAhead[i], referenceStatesAhead[i+1])
         g = vertcat(g, motionModelConstraint)
 
-    for i in range(horizon): #obstacle 1
+    for i in range(horizon):
+        #define constraint to avoid obstacle 1
         d = P[(i+1)*2:(i+1)*2+2] + referenceStatesAhead[i+1][0:2] - obstacleCenter
         g = vertcat(g, d.T @ d - (obstacleRadius+obstaclePadding)**2) 
     
-    for i in range(horizon): #obstacle 2
+    for i in range(horizon):
+        #define constraint to avoid obstacle 2
         d = P[(i+1)*2:(i+1)*2+2] + referenceStatesAhead[i+1][0:2] - obstacleCenter2
         g = vertcat(g, d.T @ d - (obstacleRadius2+obstaclePadding)**2) 
 
-    for i in range(horizon): #map bounds
+    for i in range(horizon):
+        #define map boundary constraint
         d = P[(i+1)*2:(i+1)*2+2] + referenceStatesAhead[i+1][0:2]
         g = vertcat(g, d)
 
@@ -199,11 +214,12 @@ def NLP_controller(delta_t, horizon, traj, currentIter, currentState, freeSpaceB
     "x0": initialConditionSolverConstraint
     }
 
+    #run Casadi NLP solver with constraints
     opts = {'ipopt.print_level':0, 'print_time':0}
     solver = nlpsol("solver", "ipopt", {'x':variables, 'f':costFunction, 'g':g}, opts)
-
-
     sol = solver(**solver_params)
+
+    #return the first control input of the solution sequence
     return [float(sol["x"][0]), float(sol["x"][1])]
 
 
